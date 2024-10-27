4d7ee1713c512ee07fc2c558e56c72a26f78bd70
Luccas <Lukinhas13yago@gmail.com>
2024-10-27 20:51:59 -0300

    one commit

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "Sobre", link: "#about" },
+  { name: "Projetos", link: "#projects" },
+  { name: "Testemunhas", link: "#testimonials" },
+  { name: "Contato", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Priorizo a colaboração do cliente, promovendo a comunicação aberta",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Sou muito flexível com comunicação de fuso horário",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Meu tech stack",
+    description: "Melhorando constantemante!",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de tecnologia e apaixonado por desenvolvimento",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Atualmente em busca de certificações..",
+    description: "A informação privilegiada",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Quer começar um projeto. Vamos Trabalhar Juntos ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Titulo 1",
+    des: "Descrição",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Titilo 2",
+    des: "Descrição aqui",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Titulo 3",
+    des: "Descrição",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Website Apple iphone 15 pro",
+    des: "Recriando o site da Apple usando UX, e combiando GSAP e Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Colaborar com Luccas foi um prazer absoluto. Seu profissionalismo, presteza e dedicação na entrega de resultados excepcionais ficaram evidentes ao longo do nosso projeto. O entusiasmo de Luccas por cada faceta do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Luccas é o parceiro ideal.",
+    name: "Luccas Stefani",
+    title: "Engenherio de Software",
+  },
+  {
+    quote:
+      "Colaborar com Luccas foi um prazer absoluto. Seu profissionalismo, presteza e dedicação na entrega de resultados excepcionais ficaram evidentes ao longo do nosso projeto. O entusiasmo de Luccas por cada faceta do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Luccas é o parceiro ideal.",
+    name: "Luccas Stefani",
+    title: "Engenherio de Software",
+  },
+  {
+    quote:
+      "Colaborar com Luccas foi um prazer absoluto. Seu profissionalismo, presteza e dedicação na entrega de resultados excepcionais ficaram evidentes ao longo do nosso projeto. O entusiasmo de Luccas por cada faceta do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Luccas é o parceiro ideal.",
+    name: "Luccas Stefani",
+    title: "Engenherio de Software",
+  },
+  {
+    quote:
+      "Colaborar com Luccas foi um prazer absoluto. Seu profissionalismo, presteza e dedicação na entrega de resultados excepcionais ficaram evidentes ao longo do nosso projeto. O entusiasmo de Luccas por cada faceta do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Luccas é o parceiro ideal.",
+    name: "Luccas Stefani",
+    title: "Engenherio de Software",
+  },
+  {
+    quote:
+      "Colaborar com Luccas foi um prazer absoluto. Seu profissionalismo, presteza e dedicação na entrega de resultados excepcionais ficaram evidentes ao longo do nosso projeto. O entusiasmo de Luccas por cada faceta do desenvolvimento realmente se destaca. Se você busca elevar seu site e sua marca, Luccas é o parceiro ideal.",
+    name: "Luccas Stefani",
+    title: "Engenherio de Software",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
